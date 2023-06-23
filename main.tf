1cb71012f06a304a5616f40c10a5bd73bb646096
Vincent Burckhardt <vincent.burckhardt@ie.ibm.com>
2023-06-23 14:07:39 +0100

    no-api

--- main.tf
+++ main.tf
@@ -1,19 +1,11 @@
-variable "ibmcloud_api_key" {
-  type        = string
-  description = "The IBM Cloud api token"
-  sensitive   = true
-}
-
 
 provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
   region           = "eu-gb"
 }
 
 terraform {
   required_version = ">=1.3.0"
   required_providers {
-    # Pin to the lowest provider version of the range defined in the main module to ensure lowest version still works
     ibm = {
       source  = "ibm-cloud/ibm"
       version = "1.51.0"
@@ -23,4 +15,5 @@ terraform {
 
 data "ibm_container_cluster_config" "cluster_config" {
   cluster_name_id   = "osco-exp"
+  admin = true
 }
